925ef99541582251bf396a6c22fb21843a0f52c0
Jaxon Johnson <jaxoncodes@gmail.com>
2022-08-04 11:20:35 -0400

    Initial Commit, package setup and backend

---
+++ server/server.js
@@ -0,0 +1,19 @@
+// DEPENDENCIES 
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config({path: './config.env'});
+const port = process.env.PORT || 5000; // If can't find the right port from env file, use 5000 instead using the OR operator.
+
+app.use(cors());
+app.use(express.json());
+app.use(require('./routes/record'));
+
+const dbo = require('./db/conn')
+
+app.listen(port, () => {
+    dbo.connectToServer(function (err) {
+        if (err) console.error(err);
+    });
+    console.log(`Server is running on port ${port}`)
+});
